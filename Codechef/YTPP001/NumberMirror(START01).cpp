9f3dbc1d2be151130a793bdcc28924d0d15ee578
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2021-06-14 09:09:50 +0530

    Added Codechef/START01 - Number Mirror

---
+++ Codechef/YTPP001/NumberMirror(START01).cpp
@@ -0,0 +1,16 @@
+/*
+* Codechef YTPP001
+* Problem: START01 - Number Mirror
+* Author: Hariket Sukeshkumar Sheth
+* Time Limit: 1 second
+* Link: https://www.codechef.com/YTPP001/problems/START01
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test_case;
+    cin >> test_case;
+    cout<< test_case << endl;
+}
